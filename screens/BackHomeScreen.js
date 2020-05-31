534de828f3f313184c3d2ac82621c773fbd61d08
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-05-31 16:47:44 -0300

    Add assets to NeedToScreen

--- screens/BackHomeScreen.js
+++ screens/BackHomeScreen.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import * as colors from '../theme/colors';
-import { Content, Text, Thumbnail } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Content, Text, Thumbnail } from 'native-base';
 
 const BackHomeScreen = () => {
   return (
     <Content style={{ backgroundColor: colors.secondary }} padder>
-      <Text style={{ fontFamily: 'museo', fontSize: 24, textAlign: 'center', marginTop: 20 }}>When you get home don't forget to ...</Text>
+      <Text style={{ fontFamily: 'museo', fontSize: 24, textAlign: 'center', marginTop: 20 }}>When you get home, don't forget to ...</Text>
       <Grid style={{ backgroundColor: colors.primary, alignItems: 'center', marginTop: 25, marginLeft: -10, marginRight: -10 }}>
         <Col style={{ paddingTop: 10, paddingBottom: 10 }}>
           <Thumbnail source={require('../assets/shoe.png')} style={{ width: 325, marginTop: 10 }} />
           <Text style={{ color: 'white', fontFamily: 'museo', textAlign: 'center' }}>Take off your shoes</Text>
-          <Thumbnail source={require('../assets/washing-machine.png')} style={{ width: 359, height: 100, marginTop: 30 }} />
+          <Thumbnail source={require('../assets/washing-machine.png')} style={{ width: 370, height: 100, marginTop: 30 }} />
           <Text style={{ color: 'white', fontFamily: 'museo', textAlign: 'center' }}>Wash your clothes, mask</Text>
-          <Thumbnail source={require('../assets/washing-hands.png')} style={{ width: 350, height: 120, marginTop: 10 }} />
+          <Thumbnail source={require('../assets/washing-hands.png')} style={{ width: 360, height: 120, marginTop: 10 }} />
           <Text style={{ color: 'white', fontFamily: 'museo', textAlign: 'center' }}>And your hands</Text>
         </Col>
       </Grid>
--- screens/NeedToScreen.js
+++ screens/NeedToScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import * as colors from '../theme/colors';
-import { Content, Text } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Content, Text, Thumbnail } from 'native-base';
 
 const NeedToScreen = () => {
   return (
     <Content style={{ backgroundColor: colors.secondary }} padder>
-      <Text>NeedToScreen</Text>
+      <Text style={{ fontFamily: 'museo', fontSize: 24, textAlign: 'center', marginTop: 20 }}>When you leave home, you need to ...</Text>
+      <Grid style={{ backgroundColor: colors.primary, alignItems: 'center', marginTop: 25, marginLeft: -10, marginRight: -10 }}>
+        <Col style={{ paddingTop: 10, paddingBottom: 10 }}>
+          <Thumbnail source={require('../assets/mask-icon.png')} style={{ width: 380, height: 80, marginTop: 30, marginBottom: -20 }} />
+          <Text style={{ color: 'white', fontFamily: 'museo', textAlign: 'center' }}>Wash your clothes, mask</Text>
+          <Thumbnail source={require('../assets/washing-hands.png')} style={{ width: 360, height: 120, marginTop: 10 }} />
+          <Text style={{ color: 'white', fontFamily: 'museo', textAlign: 'center' }}>Wash your hands</Text>
+        </Col>
+      </Grid>
     </Content>
   );
 };
