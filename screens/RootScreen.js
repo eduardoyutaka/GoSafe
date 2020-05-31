c1aeb6a46e843293162ca04860214afe4c152c45
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-05-31 11:27:14 -0300

    Set styles to Tabs

--- screens/RootScreen.js
+++ screens/RootScreen.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as colors from '../theme/colors';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Container, Header, Tab, Tabs, TabHeading, Icon, Text } from 'native-base';
 import NeedTo from './NeedToScreen';
 import WhereTo from './WhereToScreen';
@@ -7,15 +9,60 @@ import BackHome from './BackHomeScreen';
 const RootScreen = () => {
   return (
     <Container>
-      <Header hasTabs />
-      <Tabs>
-        <Tab heading={ <TabHeading><Icon name="medkit" /><Text>NEED TO</Text></TabHeading> }>
+      <Header style={{ backgroundColor: colors.secondary }} hasTabs />
+      <Tabs tabBarUnderlineStyle={{ borderBottomWidth: '5px' }}>
+        <Tab heading={
+          <TabHeading style={{ backgroundColor: colors.secondary }}>
+            <Grid>
+              <Row>
+                <Col>
+                  <Icon name="medkit" style={{ color: 'black', textAlign: 'center' }} />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Text style={{ color: 'black', fontFamily: 'museo', fontSize: 12, textAlign: 'center' }}>NEED TO</Text>
+                </Col>
+              </Row>
+            </Grid>
+          </TabHeading>
+        }>
           <NeedTo />
         </Tab>
-        <Tab heading={ <TabHeading><Icon name="navigate" /><Text>WHERE TO</Text></TabHeading> }>
+        <Tab heading={
+          <TabHeading style={{ backgroundColor: colors.secondary }}>
+            <Grid>
+              <Row>
+                <Col>
+                  <Icon name="navigate" style={{ color: 'black', textAlign: 'center' }} />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Text style={{ color: 'black', fontFamily: 'museo', fontSize: 12, textAlign: 'center' }}>WHERE TO</Text>
+                </Col>
+              </Row>
+            </Grid>
+          </TabHeading>
+        }>
           <WhereTo />
         </Tab>
-        <Tab heading={ <TabHeading><Icon name="home" /><Text>BACK HOME</Text></TabHeading> }>
+        <Tab heading={
+          <TabHeading style={{ backgroundColor: colors.secondary }}>
+            <Grid>
+              <Row>
+                <Col>
+                  <Icon name="navigate" style={{ color: 'black', textAlign: 'center' }} />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Text style={{ color: 'black', fontFamily: 'museo', fontSize: 12, textAlign: 'center' }}>BACK HOME</Text>
+                </Col>
+              </Row>
+            </Grid>
+          </TabHeading>
+        }>
           <BackHome />
         </Tab>
       </Tabs>
