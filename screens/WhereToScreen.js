6e23fe7a54c7a09746c9216899fd78e8eeb6bcad
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-05-31 16:40:39 -0300

    Add react-native-maps to WhereToScreen

--- screens/WhereToScreen.js
+++ screens/WhereToScreen.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import * as colors from '../theme/colors';
-import { Content, Form, Input, Item, Text } from 'native-base';
+import MapView from 'react-native-maps';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Button, Content, Form, Icon, Input, Item, Text } from 'native-base';
 
 const WhereToScreen = () => {
   return (
-    <Content style={{ backgroundColor: colors.secondary }} padder>
-      <Form>
+    <Content style={{ backgroundColor: colors.secondary }} contentContainerStyle={{ alignItems: 'center' }} padder>
+      <Form >
         <Text style={{ fontFamily: 'museo', textAlign: 'center', paddingTop: 20, paddingBottom: 20, fontSize: 24 }}>Where do you NEED to go?</Text>
-        <Item regular style={{ backgroundColor: 'white', borderRadius: 10 }}>
+        <Item regular style={{ backgroundColor: 'white', borderRadius: 10, marginBottom: 30 }}>
           <Input />
+          <Icon name="search" />
         </Item>
+        <MapView style={{ width: 310, height: 310 }} />
+        <Button style={{ backgroundColor: colors.primary, justifyContent: 'center', marginTop: 30 }}>
+          <Text style={{ color: 'black', fontFamily: 'museo', textAlign: 'center' }}>Going now!</Text>
+        </Button>
       </Form>
     </Content>
   );
